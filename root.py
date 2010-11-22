c5246717c171c6c201719980aedaf3243d688041
J. David Ibanez <jdavid@itaapy.com>
2010-11-22 15:04:35 +0100

    Every project has its own documentation

--- root.py
+++ root.py
@@ -17,24 +17,17 @@
 # Import from itools
 from itools.database import AndQuery, PhraseQuery
 from itools.datatypes import Email, String
-from itools.fs import FileName
 from itools.gettext import MSG
-from itools.handlers import get_handler_class_by_mimetype
-from itools.html import XHTMLFile
-from itools.stl import stl, rewrite_uris
-from itools.uri import get_reference
-from itools.web import STLView, BaseForm, FormError
-from itools.xml import get_element, TEXT
+from itools.stl import stl
+from itools.web import STLView, BaseForm
 
 # Import from ikaaro
-from ikaaro.autoform import AutoForm, FileWidget
-from ikaaro.buttons import Button
 from ikaaro.blog.blog import Blog, Post
-from ikaaro.datatypes import FileDataType
-from ikaaro.folder import Folder
-from ikaaro.messages import MSG_UNEXPECTED_MIMETYPE
 from ikaaro.root import Root as BaseRoot
 from ikaaro.website import WebSite
+
+# Import from hforge
+from project import Project
 
 
 ###########################################################################
@@ -148,115 +141,17 @@ class Root_Subscribe(BaseForm):
 
 
 
-class Root_UpdateDocs(AutoForm):
-
-    access = 'is_admin'
-    title = MSG(u'Update docs')
-
-    schema = {
-        'file': FileDataType(mandatory=True)}
-    widgets = [
-        FileWidget('file', title=MSG(u'File'))]
-    actions = [
-        Button(access='is_admin', css='button-ok', title=MSG(u'Upload'))]
-
-
-    def _get_form(self, resource, context):
-        form = super(Root_UpdateDocs, self)._get_form(resource, context)
-        # Check the mimetype
-        filename, mimetype, body = form['file']
-        if mimetype not in ('application/x-tar', 'application/zip'):
-            raise FormError, MSG_UNEXPECTED_MIMETYPE(mimetype=mimetype)
-
-        return form
-
-
-    def action(self, resource, context, form):
-        skip = set(['application/javascript', 'application/octet-stream',
-                    'text/css', 'text/plain'])
-        keep = set(['application/pdf', 'image/png'])
-
-        def rewrite(value):
-            if value[0] == '#':
-                return value
-            ref = get_reference(value)
-            if ref.scheme:
-                return value
-            name = ref.path.get_name()
-            name, extension, langage = FileName.decode(name)
-            if extension in ('png', 'pdf'):
-                name = '%s/;download' % name
-            ref.path[-1] = name
-            return '../%s' % ref
-
-        def filter(path, mimetype, body):
-            # HTML
-            if mimetype == 'text/html':
-                source = XHTMLFile(string=body)
-                target = XHTMLFile()
-                elem = get_element(source.events, 'div', **{'class': 'body'})
-                if not elem:
-                    print 'E', path
-                    return None
-                elements = elem.get_content_elements()
-                elements = rewrite_uris(elements, rewrite)
-                elements = list(elements)
-                target.set_body(elements)
-                return target.to_str()
-            # Skip
-            elif mimetype in skip:
-                return None
-            # Keep
-            elif mimetype in keep:
-                return body
-            # Unknown
-            else:
-                print 'X', path, mimetype
-                return body
-
-        def postproc(file):
-            # State
-            file.set_property('state', 'public')
-            # Title
-            if file.class_id != 'webpage':
-                return
-            handler = file.get_handler()
-            events = handler.events
-            elem = get_element(events, 'h1')
-            title = [
-                unicode(x[1], 'utf8') for x in elem.get_content_elements()
-                if x[0] == TEXT ]
-            if title[-1] == u'¶':
-                title.pop()
-            title = u''.join(title)
-            file.set_property('title', title, 'en')
-            handler.events = events[:elem.start] + events[elem.end+1:]
-
-        # 1. Make the '/docs/' folder
-        resource.del_resource('docs', soft=True)
-        docs = resource.make_resource('docs', Folder)
-        # 2. Extract
-        filename, mimetype, body = form['file']
-        cls = get_handler_class_by_mimetype(mimetype)
-        handler = cls(string=body)
-        docs.extract_archive(handler, 'en', filter, postproc)
-
-        # Ok
-        message = MSG(u'Documentation updated.')
-        return context.come_back(message, goto='/docs')
-
-
 
 ###########################################################################
 # Resource
 ###########################################################################
-class Root(BaseRoot):
+class Root(Project, BaseRoot):
 
     class_id = 'hforge.org'
     class_version = '20100708'
     class_title = MSG(u'HForge')
     class_skin = 'ui/hforge'
-    class_views = BaseRoot.class_views + ['upload']
+    class_views = BaseRoot.class_views + ['update_docs']
     __fixed_handlers__ = BaseRoot.__fixed_handlers__ + ['news']
 
 
@@ -274,4 +169,3 @@ class Root(BaseRoot):
     news = Root_News()
     projects = Root_Projects()
     subscribe = Root_Subscribe()
-    upload = Root_UpdateDocs()
